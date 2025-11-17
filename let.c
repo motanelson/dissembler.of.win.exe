bf4b9623acd241d5347b6d14aef788d111e2c150
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-11-17 11:41:49 +0000

    Add files via upload

--- let.c
+++ let.c
@@ -1,7 +1,16 @@
 asm (".global _var16k \n_var16k:\n .space 16384");
 extern char *_var16k;
+char *allocvar; 
+char *allocs(int s){
+    char *a=allocvar;
+    allocvar=allocvar+s+1;
+    return a;
+
+}
 int _start(){
-    char *a=_var16k;    
+    allocvar=_var16k+0;
+    char *a=allocs(100);
+    char *b=allocs(100);    
     return 0;
 
 }
--- let.s
+++ let.s
@@ -9,19 +9,61 @@
 _var16k:
  .space 16384
  #NO_APP
+	.globl	allocvar
+	.section	.sbss,"aw",@nobits
+	.align	3
+	.type	allocvar, @object
+	.size	allocvar, 8
+allocvar:
+	.zero	8
+	.text
+	.align	1
+	.globl	allocs
+	.type	allocs, @function
+allocs:
+	addi	sp,sp,-48
+	sd	s0,40(sp)
+	addi	s0,sp,48
+	mv	a5,a0
+	sw	a5,-36(s0)
+	lui	a5,%hi(allocvar)
+	ld	a5,%lo(allocvar)(a5)
+	sd	a5,-24(s0)
+	lui	a5,%hi(allocvar)
+	ld	a4,%lo(allocvar)(a5)
+	lw	a5,-36(s0)
+	addi	a5,a5,1
+	add	a4,a4,a5
+	lui	a5,%hi(allocvar)
+	sd	a4,%lo(allocvar)(a5)
+	ld	a5,-24(s0)
+	mv	a0,a5
+	ld	s0,40(sp)
+	addi	sp,sp,48
+	jr	ra
+	.size	allocs, .-allocs
 	.align	1
 	.globl	_start
 	.type	_start, @function
 _start:
 	addi	sp,sp,-32
-	sd	s0,24(sp)
+	sd	ra,24(sp)
+	sd	s0,16(sp)
 	addi	s0,sp,32
 	lui	a5,%hi(_var16k)
-	ld	a5,%lo(_var16k)(a5)
-	sd	a5,-24(s0)
+	ld	a4,%lo(_var16k)(a5)
+	lui	a5,%hi(allocvar)
+	sd	a4,%lo(allocvar)(a5)
+	li	a0,100
+	call	allocs
+	sd	a0,-24(s0)
+	li	a0,100
+	call	allocs
+	sd	a0,-32(s0)
 	li	a5,0
 	mv	a0,a5
-	ld	s0,24(sp)
+	ld	ra,24(sp)
+	ld	s0,16(sp)
 	addi	sp,sp,32
 	jr	ra
 	.size	_start, .-_start
