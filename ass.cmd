bf721e537007783e7c1b13a46028dcfe210f9032
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-12-08 13:04:50 +0000

    Add files via upload

--- ass.cmd
+++ ass.cmd
@@ -1,3 +1,4 @@
 %echo off
 color 60
-"C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8.1 Tools"\ildasm.exe /tok /byt %1 /out=myfile.il
+set path=C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8.1 Tools
+ildasm.exe  %1 /out=myfile.il
